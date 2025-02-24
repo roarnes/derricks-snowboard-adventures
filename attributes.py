2bc8b1a8635387bb16a4cb2557ac30255e63feb2
Arnes <respatiarnes@gmail.com>
2025-02-24 19:05:47 +0100

    feat: init derrick snowboard game with pygame

---
+++ attributes.py
@@ -0,0 +1,209 @@
+import pygame
+import random
+
+game_speed = 3
+
+
+class Tree(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.x_pos = 1380
+
+        original_image = pygame.image.load("assets/tree.png")
+        original_width, original_height = original_image.get_size()
+
+        fixed_height = random.randint(80, 200)  # Set fixed height
+        aspect_ratio = original_width / original_height  # Calculate aspect ratio
+        new_width = int(fixed_height * aspect_ratio)
+
+        # Ensure the tree bottom is always at y = 360
+        self.y_pos = 400 - (fixed_height // 2)
+
+        self.image = pygame.transform.scale(
+            original_image, (new_width, fixed_height))
+
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+
+    def update(self):
+        # self.x_pos -= game_speed
+        self.rect.x -= 1
+
+
+class Cloud(pygame.sprite.Sprite):
+    def __init__(self, x_pos, y_pos):
+        super().__init__()
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        # self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+
+        self.sprites = []
+        for i in range(1, 7):
+            original_image = pygame.image.load(f"assets/clouds/cloud{i}.png")
+
+            original_width, original_height = original_image.get_size()
+
+            fixed_height = random.randint(20, 80)  # Set fixed height
+            aspect_ratio = original_width / original_height  # Calculate aspect ratio
+            # Scale width proportionally
+            new_width = int(fixed_height * aspect_ratio)
+
+            scaled_sprite = pygame.transform.scale(
+                original_image, (new_width, fixed_height))
+            self.sprites.append(scaled_sprite)
+        self.image = random.choice(self.sprites)
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+
+    def update(self):
+        # self.x_pos -= game_speed
+        self.rect.x -= 1
+
+
+class Derrick(pygame.sprite.Sprite):
+    def __init__(self, x_pos, y_pos):
+        super().__init__()
+
+        self.ride_img = pygame.transform.scale(
+            pygame.image.load("assets/Ride.png"), (80, 120))
+        self.jump_img = pygame.transform.scale(
+            pygame.image.load(f"assets/Jump.png"), (80, 120))
+        self.duck_img = pygame.transform.scale(
+            pygame.image.load(f"assets/Duck.png"), (95, 95))
+
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.image = 0
+        self.image = self.ride_img
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+        self.velocity = 20
+        self.gravity = 3
+        self.ducking = False
+        self.jumping = False
+
+    def jump(self):
+        if not self.jumping and self.rect.centery == 360:  # Allow jump only if on ground
+            self.jumping = True
+            # Start with an initial negative velocity (jump impulse)
+            self.velocity = -6
+            jump_sfx = pygame.mixer.Sound("assets/sfx/jump.mp3")
+            jump_sfx.play()
+
+    def duck(self):
+        self.ducking = True
+        self.rect.centery = 380
+
+    def unduck(self):
+        self.ducking = False
+        self.rect.centery = 360
+
+    def apply_gravity(self):
+
+        if self.jumping:
+            self.rect.centery += self.velocity  # Move up or down
+            self.velocity += 0.1  # Simulate gravity
+
+            if self.rect.centery >= 360:  # If Derrick lands back
+                self.rect.centery = 360
+                self.jumping = False  # Reset jumping state
+                self.velocity = 0  # Reset velocity
+
+    def update(self):
+        self.animate()
+        self.apply_gravity()
+
+    def animate(self):
+        if self.ducking:
+            self.image = self.duck_img
+        elif self.jumping:
+            self.image = self.jump_img
+        else:
+            self.image = self.ride_img
+
+
+class RunningCat(pygame.sprite.Sprite):
+    def __init__(self, x_pos, y_pos):
+        super().__init__()
+
+        # Load sprite animations
+        self.buttface_sprites = [
+            pygame.transform.scale(pygame.image.load(f"assets/Buttface{i}.png"), (60, 60)) for i in range(1, 3)
+        ]
+        self.dumdum_sprites = [
+            pygame.transform.scale(pygame.image.load(f"assets/Dumdum{i}.png"), (60, 60)) for i in range(1, 3)
+        ]
+
+        # Store animations separately
+        self.sprites = [self.dumdum_sprites, self.buttface_sprites]
+        self.current_sprite_index = random.randint(0, 1)
+        self.current_image_index = 0  # Current frame in animation
+
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+
+        # Set initial image
+        self.image = self.sprites[self.current_sprite_index][self.current_image_index]
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+
+        # Animation timers
+        self.animation_speed = 0.025  # Adjust speed of animation
+        self.switch_timer = 2  # Timer for switching between dumdum & buttface
+        self.frame_timer = 0  # Timer for frame animation
+
+    def update(self):
+        self.animate()
+        self.x_pos -= game_speed
+        self.rect.center = (self.x_pos, self.y_pos)
+
+    def animate(self):
+        # Animate the selected sprite set
+        self.frame_timer += self.animation_speed
+        if self.frame_timer >= 1:  # Adjust for smooth animation
+            self.frame_timer = 0
+            self.current_image_index = (
+                self.current_image_index + 1) % len(self.sprites[self.current_sprite_index])
+
+        # Update sprite image
+        self.image = self.sprites[self.current_sprite_index][self.current_image_index]
+
+
+class FlyingCat(pygame.sprite.Sprite):
+    def __init__(self, x_pos):
+        super().__init__()
+        self.x_pos = x_pos
+        self.y_pos = random.choice([265, 275, 290])
+
+        # Load sprite animations
+        self.buttface_sprites = [
+            pygame.transform.scale(pygame.image.load(f"assets/ButtfaceFly{i}.png"), (60, 60)) for i in range(1, 3)
+        ]
+        self.dumdum_sprites = [
+            pygame.transform.scale(pygame.image.load(f"assets/DumdumFly{i}.png"), (60, 60)) for i in range(1, 3)
+        ]
+
+        # Store animations separately
+        self.sprites = [self.dumdum_sprites, self.buttface_sprites]
+        self.current_sprite_index = random.randint(0, 1)
+        self.current_image_index = 0  # Current frame in animation
+
+        # Set initial image
+        self.image = self.sprites[self.current_sprite_index][self.current_image_index]
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+
+        self.animation_speed = 0.025  # Adjust speed of animation
+        self.switch_timer = 0  # Timer for switching between dumdum & buttface
+        self.frame_timer = 0  # Timer for frame animation
+
+    def update(self):
+        self.animate()
+        self.x_pos -= game_speed
+        self.rect.center = (self.x_pos, self.y_pos)
+
+    def animate(self):
+        # Animate the selected sprite set
+        self.frame_timer += self.animation_speed
+        if self.frame_timer >= 1:  # Adjust for smooth animation
+            self.frame_timer = 0
+            self.current_image_index = (
+                self.current_image_index + 1) % len(self.sprites[self.current_sprite_index])
+
+        # Update sprite image
+        self.image = self.sprites[self.current_sprite_index][self.current_image_index]
---
+++ main.py
@@ -0,0 +1,198 @@
+
+from attributes import Derrick, Cloud, Tree, FlyingCat, RunningCat
+import pygame
+import sys
+import random
+
+pygame.init()
+
+game_speed = 5
+player_score = 0
+game_over = False
+obstacle_timer = 0
+obstacle_spawn = False
+obstacle_cooldown = 1000
+
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+pygame.display.set_caption("Derrick's Snowboard Adventures")
+game_font = pygame.font.Font("assets/PressStart2P-Regular.ttf", 24)
+
+# Objects
+tree_group = pygame.sprite.Group()
+cloud_group = pygame.sprite.Group()
+obstacle_group = pygame.sprite.Group()
+derrick_group = pygame.sprite.GroupSingle()
+
+derrick = Derrick(200, 360)
+derrick_group.add(derrick)
+
+# Surfaces
+ground = pygame.image.load("assets/ground.png")
+ground = pygame.transform.scale(ground, (1280, 360))
+ground_x = 0
+ground_y = 400
+ground_rect = ground.get_rect(center=(640, 400))
+
+# Sounds
+death_sfx = pygame.mixer.Sound("assets/sfx/lose.mp3")
+points_sfx = pygame.mixer.Sound("assets/sfx/100points.mp3")
+
+# Events
+CLOUD_EVENT = pygame.USEREVENT
+pygame.time.set_timer(CLOUD_EVENT, 700)
+
+
+TREE_EVENT = pygame.USEREVENT
+pygame.time.set_timer(TREE_EVENT, random.randint(
+    500, 1300))  # Random interval
+
+
+def reset_tree_timer():
+    pygame.time.set_timer(TREE_EVENT, random.randint(
+        500, 1300))   # Randomize next tree
+
+
+def reset_obstacles():
+    global obstacle_timer, obstacle_spawn, last_obstacle_x, obstacle_cooldown, max_gap
+    last_obstacle_x = random.randint(700, 1000)
+    max_gap = 100
+
+
+def end_game():
+    global player_score, game_speed
+    game_over_text = game_font.render("Game Over!", True, "black")
+    game_over_rect = game_over_text.get_rect(center=(640, 300))
+    score_text = game_font.render(f"Score: {int(player_score)}", True, "black")
+    score_rect = score_text.get_rect(center=(640, 340))
+    screen.blit(game_over_text, game_over_rect)
+    screen.blit(score_text, score_rect)
+    game_speed = 5
+    tree_group.empty()
+    cloud_group.empty()
+    obstacle_group.empty()
+
+    reset_obstacles()
+
+
+# Initial value to prevent early overlap
+last_obstacle_x = random.randint(700, 1000)
+min_gap = 30  # Minimum distance between obstacles
+max_gap = 100  # Maximum spacing for variety
+
+
+def spawn_obstacle():
+    global obstacle_timer, obstacle_spawn, last_obstacle_x, obstacle_cooldown, max_gap
+
+    if pygame.time.get_ticks() - obstacle_timer >= obstacle_cooldown:
+        obstacle_spawn = True
+
+        # Reduce obstacle cooldown as the game progresses
+        # Prevent it from getting too fast
+        if obstacle_cooldown > 500:
+            obstacle_cooldown -= 1
+        if max_gap > min_gap + 5:
+            max_gap -= 0.08
+
+    if obstacle_spawn:
+        # Calculate the next obstacle position based on the last obstacle
+        spawn_x = last_obstacle_x + random.randint(min_gap, int(max_gap))
+
+        obstacle_random = random.randint(1, 50)
+        if obstacle_random in range(1, 8):
+            # Running cat at ground level
+            new_obstacle = RunningCat(spawn_x, 380)
+            obstacle_group.add(new_obstacle)
+
+            # Update last obstacle position
+            last_obstacle_x = spawn_x
+            obstacle_timer = pygame.time.get_ticks()
+            obstacle_spawn = False
+        elif obstacle_random in range(30, 34):
+            # Flying cat at a random height
+            new_obstacle = FlyingCat(spawn_x)
+            obstacle_group.add(new_obstacle)
+
+            # Update last obstacle position
+            last_obstacle_x = spawn_x
+            obstacle_timer = pygame.time.get_ticks()
+            obstacle_spawn = False
+
+
+while True:
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_DOWN]:
+        derrick.duck()
+    elif keys[pygame.K_SPACE] or keys[pygame.K_UP]:
+        derrick.jump()
+    else:
+        if derrick.ducking:
+            derrick.unduck()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == CLOUD_EVENT:
+            current_cloud_y = random.randint(50, 300)
+            current_cloud = Cloud(1380, current_cloud_y)
+            cloud_group.add(current_cloud)
+        if event.type == TREE_EVENT:
+            if random.random() < 0.6:  # 60% chance to spawn a tree
+                current_tree = Tree()
+                tree_group.add(current_tree)
+
+            reset_tree_timer()  # Set the next random tree event
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE or event.key == pygame.K_UP:
+                derrick.jump()
+                if game_over:
+                    game_over = False
+                    game_speed = 5
+                    player_score = 0
+    screen.fill("#EAF7FF")
+
+    # Collisions
+    if pygame.sprite.spritecollide(derrick_group.sprite, obstacle_group, False):
+        game_over = True
+        death_sfx.play()
+    if game_over:
+        end_game()
+
+    if not game_over:
+        game_speed += 0.000025
+        if round(player_score, 1) % 100 == 0 and int(player_score) > 0:
+            points_sfx.play()
+
+        spawn_obstacle()
+
+        player_score += 0.1
+        player_score_surface = game_font.render(
+            str(int(player_score)), True, ("black"))
+        screen.blit(player_score_surface, (1150, 10))
+
+        screen.blit(ground, (ground_x, ground_y))
+        screen.blit(ground, (ground_x + 1280, ground_y))
+
+        if ground_x <= -1280:
+            ground_x = 0
+
+        # player_score_surface = game_font.render(
+        #     str('Happy Birthday Derrick'), True, ("black"))
+        # screen.blit(player_score_surface, (300, 150))
+
+        cloud_group.update()
+        cloud_group.draw(screen)
+
+        tree_group.update()
+        tree_group.draw(screen)
+
+        derrick_group.update()
+        derrick_group.draw(screen)
+
+        obstacle_group.update()
+        obstacle_group.draw(screen)
+
+        ground_x -= game_speed
+
+    clock.tick(120)
+    pygame.display.update()
